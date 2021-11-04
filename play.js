1e1aee51d9b5256aa94857b84579009096fb7af5
Justin Tu <coppolavina@gmail.com>
2021-11-04 02:08:21 +0000

    setupInput and handleUserInput function

--- play.js
+++ play.js
@@ -14,16 +14,33 @@ const connect = function () {
     conn.write('Name: JTT')
     setTimeout(() => conn.write("Move: up"),50);
   });
-  
-  
 
   conn.on("data", () => {
     console.log(`you ded cuz you idled`);
   });
   
-  return conn;
-
+return conn;
 };
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function(key) {
+    if (key === '\u0003' ) {
+      process.exit();}
+    }
+  stdin.on("data", handleUserInput);
+
+  
+  return stdin;
+};
+
+setupInput()
+
+
+
 module.exports = {connect};
 
 console.log("Connecting ...");
